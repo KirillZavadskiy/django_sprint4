4b5f7856372ad754d36022b52d47f580894acded
Кирилл Завадский <zavad93@mail.ru>
2023-09-17 21:17:26 +0300

    Fixed review + isort.

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,8 +3,6 @@ from django.db.models import Count, Q
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
-from django.db.models import Q
-
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
 
